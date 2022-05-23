07c25f5c7ef314aaee7f1969f852a482772e63ee
Yeo <guswls9281@gmail.com>
2022-05-23 16:33:02 +0900

    sklearn 문제 해결

--- app_ml.py
+++ app_ml.py
@@ -5,7 +5,7 @@ import numpy as np
 def run_ml():
     st.subheader('자동차 구매 가능 금액 예측')
     
-    print(sklearn.__version)
+    #print(sklearn.__version)
     # 예측하기 위해서 필요한 파일들을 불러와야 된다.
     # 이 예에서는, 인공지능 파일, X 스케일러 파일, y 스케일러 파일
     # 3개를 불러와야 한다. 
